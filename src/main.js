834648c717c90bd5745a6139cdae14a497eef03e
Gene Park <gejpark@ucsc.edu>
2023-04-25 16:25:25 -0700

    comments

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@
 //  Was very simple, just removed if(!isFiring) check.
 //  - Implement the 'FIRE' UI text from the original game (5)
 //  Added new text component. Only make visible alpha when isFiring.
+//  TOTAL POINT VALUE: 
 //CITATIONS:
+
+//notes:
+// for high score that persists across screens, just get the highest score value and attach it to the UI (above border, where FIRE is)
 
 let config = {
     type: Phaser.AUTO, //Phaser.CANVAS => webgl isn't pixel perfect when rendering for some reason
