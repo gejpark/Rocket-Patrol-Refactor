3ba59a4a4d72ad4e99f3fd30b94c44a8ec2c4c55
gejpark <122942803+gejpark@users.noreply.github.com>
2023-04-30 10:17:01 -0700

    Create a new title screen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -16,18 +16,22 @@ class Menu extends Phaser.Scene {
 
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
         this.load.audio('sfx_play', './assets/space-invaders-classic-arcade-game-116826.mp3'); //play scene music
+
+        //rocket patrol menu screen
+        this.load.image('menuScreen', './assets/rocket_patrol_title_Screen.png');
     }
 
     create() {
+        this.menuImage = this.add.tileSprite(0, 0, 640, 480, 'menuScreen').setOrigin(0, 0);
         // this.add.text(20, 20, "Rocket Patrol Menu");
         // this.scene.start("playScene");
 
         // menu text configuration
         let menuConfig = {
-            fontFamily: 'Courier',
+            fontFamily: 'Trebuchet MS',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            backgroundColor: '#816271',
+            color: '#c3a38a',
             align: 'right',
             padding: {
                 top: 5,
@@ -37,11 +41,12 @@ class Menu extends Phaser.Scene {
         }
 
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/4 - borderUISize - borderPadding, 'ROCKET PATROL 2: THE QUICKENING', menuConfig).setOrigin(0.5);
+        // menuConfig.backgroundColor = '#00FF00';
+        // menuConfig.color = '#000';
+        menuConfig.fontSize = '14px';
+        this.add.text(game.config.width*(3/4), game.config.height*(3/4), 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width*(3/4), game.config.height*(3/4) + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
